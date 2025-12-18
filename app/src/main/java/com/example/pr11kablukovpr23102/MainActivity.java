338e83c586a94044ce21c145cfb542fa253ed0be
Server\NOT/founD <purplefenix228@mail.ru>
2025-12-18 13:12:26 +0700

    Create animation meteor

--- app/src/main/java/com/example/pr11kablukovpr23102/MainActivity.java
+++ app/src/main/java/com/example/pr11kablukovpr23102/MainActivity.java
@@ -21,22 +21,22 @@ public class MainActivity extends AppCompatActivity {
         ImageView hourImageView = findViewById(R.id.hour_hand);
         ImageView cloud1ImageView = findViewById(R.id.cloud1);
         ImageView cloud2ImageView = findViewById(R.id.cloud2);
+        ImageView meteorImageView = findViewById(R.id.meteor);
 
-        // 1. Восход солнца (сразу начинается)
         Animation sunRiseAnimation = AnimationUtils.loadAnimation(this, R.anim.sun_rise);
         sunImageView.startAnimation(sunRiseAnimation);
 
-        // 3. Движение первого облака (через 4 секунды)
         Animation cloud1MoveAnimation = AnimationUtils.loadAnimation(this, R.anim.cloud1_move);
         cloud1ImageView.startAnimation(cloud1MoveAnimation);
 
-        // 4. Движение второго облака (через 6 секунд)
         Animation cloud2MoveAnimation = AnimationUtils.loadAnimation(this, R.anim.cloud2_move);
         cloud2ImageView.startAnimation(cloud2MoveAnimation);
 
-        // 5. Заход солнца (через 10 секунд)
 //        Animation sunSetAnimation = AnimationUtils.loadAnimation(this, R.anim.sun_set);
 //        sunImageView.startAnimation(sunSetAnimation);
+
+        Animation meteorFallAnimation = AnimationUtils.loadAnimation(this, R.anim.meteor_fall);
+        meteorImageView.startAnimation(meteorFallAnimation);
 
         Animation clockTurnAnimation = AnimationUtils.loadAnimation(this, R.anim.clock_turn);
         clockImageView.startAnimation(clockTurnAnimation);
